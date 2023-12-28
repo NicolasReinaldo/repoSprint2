7d1ffcfff43f659d9fbcf873162abf495a08b420
nreinaldo <nicolas.reinaldo@mercadolibre.com>
2023-12-28 11:50:30 -0300

    feature: T-0003 followers list order test

--- src/test/java/meli/bootcamp/sprint1/utils/Factory.java
+++ src/test/java/meli/bootcamp/sprint1/utils/Factory.java
@@ -92,4 +92,8 @@ public class Factory {
     public static User generateSellerNoFollowers(){
         return new User(3,"Tiago",true, List.of(generatePost()), Collections.emptyList(), Collections.emptyList());
     }
+
+    public static User generateUser2(){
+        return new User(4,"Fatima",false, Collections.emptyList(),List.of(1),List.of(1,2));
+    }
 }
