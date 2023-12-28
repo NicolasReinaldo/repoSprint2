eadc38d90e1bde07d663cffed64b2dacd5646fea
nreinaldo <nicolas.reinaldo@mercadolibre.com>
2023-12-28 10:17:25 -0300

    feature: T-0008 BadRequestExceptionTest

--- src/test/java/meli/bootcamp/sprint1/unit/service/T_0008.java
+++ src/test/java/meli/bootcamp/sprint1/unit/service/T_0008.java
@@ -4,6 +4,7 @@ package meli.bootcamp.sprint1.unit.service;
 import meli.bootcamp.sprint1.dto.response.LastPostsDto;
 
 import meli.bootcamp.sprint1.entity.User;
+import meli.bootcamp.sprint1.exception.BadRequestException;
 import meli.bootcamp.sprint1.repository.impl.GeneralRepository;
 import meli.bootcamp.sprint1.service.impl.UserService;
 import org.junit.jupiter.api.DisplayName;
@@ -15,6 +16,8 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import static meli.bootcamp.sprint1.utils.Factory.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -44,5 +47,17 @@ public class T_0008 {
 
         //Assert
         assertEquals(expected,lastPostsDto);
+    }
+
+    @Test
+    @DisplayName("T-0008/ Verify exception test ")
+    void T_0008BadRequestExceptionTest(){
+
+        //Arrange
+        when(repository.findUserById(anyInt())).thenReturn(null);
+
+        //Act
+        //Assert
+        assertThrows(BadRequestException.class, () -> service.getLastPostsFromFollowed(20));
     }
 }
